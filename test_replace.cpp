bd34d330784ae453f239c07545e76ff2f73f9ae4
mdjahidulhamid <jahidulhamid@yahoo.com>
2015-12-20 06:12:26 +0600

    update-test

--- test_replace.cpp
+++ test_replace.cpp
@@ -17,7 +17,7 @@ int main(){
     std::string s="(I am a digit 67d আ 89d 4323d 3432D)";
     
     try{std::cout<<"\nreplaced string: "<<re.replace(s,"$1$22${name}","xE",2);}
-    ///                            subject^  ^replacement    ^modifiers.
+    ///                                       subject^  ^replacement    ^modifiers.
     ///2 is the length of the returned string. Though, it will be expanded as needed, pass a large enough value to contain the string,
     ///otherwise internal substitute function will be called twice which will eat up some additional resource.
     catch(int e){std::cout<<re.getErrorMessage(e);}
