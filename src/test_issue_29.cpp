81ebb53a2989e21dc7c01210c391441b64981ded
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2020-08-20 12:16:52 +0600

    Preparing for new version release

---
+++ src/test_issue_29.cpp
@@ -0,0 +1,37 @@
+/** @file test_issue_29.cpp
+ *  Test cases for issue 29 on Github.
+ *  @include test_issue_29.cpp
+ * @author [Md Jahidul Hamid](https://github.com/neurobin)
+ * @author [Moritz Bunkus](https://github.com/mbunkus)
+ *  */
+
+#include <cassert>
+#include <iostream>
+#include "jpcre2.hpp"
+
+typedef jpcre2::select<char> jp; 
+
+void test_match(jp::Regex &re, std::string const &subject) {
+    jp::RegexMatch rm;
+    jp::VecNum matches;
+    auto num = rm
+        .setRegexObject(&re)
+        .setNumberedSubstringVector(&matches)
+        .setSubject(subject)
+        .match();
+    assert(num > 0);
+    assert(matches[0][1] != "dummy"); // idx 1 should be accessible
+    assert(matches[0][2] != "dummy"); // idx 2 should be accessible
+    assert(matches[0][3] != "dummy"); // idx 3 should be accessible
+}
+
+int main(){
+    jp::Regex re("^(\\d+)([abc])?([XYZ])?$");
+
+    test_match(re, "42bZ");
+    test_match(re, "23Z");
+    test_match(re, "54b");
+    test_match(re, "13");
+    
+    return 0;
+}
